969ec10c706a17d4429adb499ad6126b0b2f7c20
Adam Visser <adamvissers@gmail.com>
2013-11-27 00:43:14 -0700

    This was an update to some of the structure and base functionality. As of now there are some settings pages added so thats exciting!

--- radatomautoload.php
+++ radatomautoload.php
@@ -1,13 +1,17 @@
 <?php
-require_once('helpers/pages.php');
-require_once('helpers/excerpts.php');
-require_once('helpers/posts.php');
-require_once('helpers/security.php');
+
 
 class RadAtomWordpressAutoload {
 	function __construct() {
-       //this probably wont be needed, were trying to do everyt
+    	require_once('helpers/pages.php');
+		require_once('helpers/excerpts.php');
+		require_once('helpers/posts.php');
+		require_once('helpers/security.php');
    }
 
+   
 }
+
+if( is_admin() )
+    $my_settings_page = new RadAtomWordpressAutoload();
 
